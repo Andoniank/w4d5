5cd76c9f3033c8f29e1f5f82a24fcaa4f8e951af
David <2018dpollack@gmail.com>
2023-08-11 16:32:12 -0700

    finish towers_of_hanoi

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tower_of_hanoi.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tower_of_hanoi.rb
@@ -3,16 +3,22 @@ class TowersOfHanoi
 
     def initialize
         @game = [[0, 1, 2], [], []]
+        self.print
     end
 
     def move(start, finish)
         disk = self.game[start].pop
         raise "Can't move from empty pile" if disk.nil?
         self.game[finish].push(disk)
+        self.print
     end
 
     def won?
         return true if self.game == [[], [], [0,1,2]]
         false
+    end
+
+    def print
+        self.game.each {|row| p row}
     end
 end
