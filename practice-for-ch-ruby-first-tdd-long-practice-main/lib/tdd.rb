dd28a46b2af3f8e4363b53f7a61663d255f00f13
Andoniank <andoniank@gmail.com>
2023-08-11 13:53:33 -0700

    Add two_sum method and rspec

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -5,3 +5,13 @@ def unique(array)
     end
     new_arr
 end
+
+def two_sum(array)
+    new_arr = []
+    array.each_with_index do |ele, i|
+        if array.include?(-ele)
+            new_arr << [i, array.index(-ele)] unless new_arr.include?([array.index(-ele), i]) || i == array.index(-ele)
+        end   
+    end
+    new_arr
+end
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -7,4 +7,11 @@ describe "tdd" do
             expect(unique(array)).to eq([1, 2, 3])
         end
     end
+
+    describe "#two_sum" do
+        it 'returns indexes of elements that sum to zero' do
+            array = [-1, 0, 2, -2, 1]
+            expect(two_sum(array)).to eq([[0, 4], [2, 3]])
+        end
+    end
 end
